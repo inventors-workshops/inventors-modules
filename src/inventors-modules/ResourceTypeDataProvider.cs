bba220238bdcbf9079cd99f3075ce7d75a11d3eb
Christine <Christine.lam.1@outlook.com>
2014-09-11 00:16:11 -0700

    Provide DB Actions for ResourceType

---
+++ src/inventors-modules/ResourceTypeDataProvider.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebApplication1
+{
+    public class ResourceTypeDataProvider
+    {
+        //create resource
+        public int create(string _name, bool _isActive)
+        {
+            int prKey = -1;
+
+            resourcetype moduleResourceType = new resourcetype();
+
+            moduleResourceType.Name = _name;
+            moduleResourceType.IsActive = _isActive;
+
+            using (db539594002Entities db = new db539594002Entities())
+            {
+                db.resourcetype.Add(moduleResourceType);
+                db.SaveChanges();
+            }
+            prKey = moduleResourceType.PrKey;
+            
+            return prKey;
+        }
+
+        //Read
+        public resourcetype displayResourceTypes()
+        {
+            db539594002Entities db = new db539594002Entities();
+             
+            return db.resourcetype;
+        }
+
+        //update ResourceType if necessary
+        public void updateResourceType(int prKey, string _name, bool _isActive)
+        {
+            using (db539594002Entities db = new db539594002Entities())
+            {
+                resourcetype moduleResourceType = db.resourcetype.Where(m => m.PrKey == prKey).First<resourcetype>();
+
+                moduleResourceType.Name = _name;
+                moduleResourceType.IsActive = _isActive;
+
+                db.SaveChanges();
+            }
+        }
+
+        public bool deleteResourceType(int prKey)
+        {
+            using (db539594002Entities db = new db539594002Entities())
+            {
+                resourcetype moduleResourceType = db.resourcetype.Where(m => m.PrKey == prKey).First<resourcetype>();
+                moduleResourceType.IsActive = false;
+                db.SaveChanges();
+            }
+            return true;
+        }
+    }
+}
---
+++ src/inventors-modules/TestResourceType.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class TestResourceType : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ResourceTypeDataProvider resourceTypeDP = new ResourceTypeDataProvider();
+
+            Listing.DataSource = resourceTypeDP.displayResourceTypes();
+
+            Listing.DataBind();
+        }
+
+        protected void Listing_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            ResourceTypeDataProvider resourceTypeDP = new ResourceTypeDataProvider();
+
+            string resourceTypeKey = Listing.DataKeys[e.RowIndex].Values["PrKey"].ToString();
+
+            int key = Int32.Parse(resourceTypeKey);
+
+            bool click = resourceTypeDP.deleteResourceType(key);
+
+            Listing.DataSource = resourceTypeDP.displayResourceTypes();
+            Listing.DataBind();
+        }
+
+    }
+}
